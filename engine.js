861a96b4131d5deeb877b9ccc8d78cba7d77889e
Morphisor <a.gallitri@gmail.com>
2017-05-21 14:04:19 +0200

    lighting??

--- engine.js
+++ engine.js
@@ -47,11 +47,17 @@ var Renderer = (function () {
         this.backbufferdata[index4 + 2] = color.b * 255;
         this.backbufferdata[index4 + 3] = color.a * 255;
     };
-    Renderer.prototype.project = function (coord, transMat) {
-        var point = BABYLON.Vector3.TransformCoordinates(coord, transMat);
-        var x = point.x * this.workingWidth + this.workingWidth / 2.0;
-        var y = -point.y * this.workingHeight + this.workingHeight / 2.0;
-        return (new BABYLON.Vector3(x, y, point.z));
+    Renderer.prototype.project = function (vertex, transMat, world) {
+        var point2d = BABYLON.Vector3.TransformCoordinates(vertex.Coordinates, transMat);
+        var point3DWorld = BABYLON.Vector3.TransformCoordinates(vertex.Coordinates, world);
+        var normal3DWorld = BABYLON.Vector3.TransformCoordinates(vertex.Normal, world);
+        var x = point2d.x * this.workingWidth + this.workingWidth / 2.0;
+        var y = -point2d.y * this.workingHeight + this.workingHeight / 2.0;
+        return ({
+            Coordinates: new BABYLON.Vector3(x, y, point2d.z),
+            Normal: normal3DWorld,
+            WorldCoordinates: point3DWorld
+        });
     };
     Renderer.prototype.drawPoint = function (point, color) {
         if (point.x >= 0 && point.y >= 0 && point.x < this.workingWidth && point.y < this.workingHeight) {
@@ -66,9 +72,13 @@ var Renderer = (function () {
     Renderer.prototype.interpolate = function (min, max, gradient) {
         return min + (max - min) * this.clamp(gradient);
     };
-    Renderer.prototype.processScanLine = function (y, pa, pb, pc, pd, color) {
-        var gradient1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
-        var gradient2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+    Renderer.prototype.processScanLine = function (data, va, vb, vc, vd, color) {
+        var pa = va.Coordinates;
+        var pb = vb.Coordinates;
+        var pc = vc.Coordinates;
+        var pd = vd.Coordinates;
+        var gradient1 = pa.y != pd.y ? (data.currentY - pa.y) / (pd.y - pa.y) : 1;
+        var gradient2 = pc.y != pd.y ? (data.currentY - pc.y) / (pd.y - pc.y) : 1;
         var sx = this.interpolate(pa.x, pb.x, gradient1) >> 0;
         var ex = this.interpolate(pc.x, pd.x, gradient2) >> 0;
         var z1 = this.interpolate(pa.z, pb.z, gradient1);
@@ -76,50 +86,70 @@ var Renderer = (function () {
         for (var x = sx; x < ex; x++) {
             var gradient = (x - sx) / (ex - sx);
             var z = this.interpolate(z1, z2, gradient);
-            this.drawPoint(new BABYLON.Vector3(x, y, z), color);
+            var ndotl = data.ndotla;
+            this.drawPoint(new BABYLON.Vector3(x, data.currentY, z), new BABYLON.Color4(color.r * ndotl, color.g * ndotl, color.b * ndotl, 1));
         }
     };
-    Renderer.prototype.drawTriangle = function (p1, p2, p3, color) {
-        if (p1.y > p2.y) {
-            var temp = p2;
-            p2 = p1;
-            p1 = temp;
+    Renderer.prototype.computeNDotL = function (vertex, normal, lightPosition) {
+        var lightDirection = lightPosition.subtract(vertex);
+        normal.normalize();
+        lightDirection.normalize();
+        return Math.max(0, BABYLON.Vector3.Dot(normal, lightDirection));
+    };
+    Renderer.prototype.drawTriangle = function (v1, v2, v3, color) {
+        if (v1.Coordinates.y > v2.Coordinates.y) {
+            var temp = v2;
+            v2 = v1;
+            v1 = temp;
         }
-        if (p2.y > p3.y) {
-            var temp = p2;
-            p2 = p3;
-            p3 = temp;
+        if (v2.Coordinates.y > v3.Coordinates.y) {
+            var temp = v2;
+            v2 = v3;
+            v3 = temp;
         }
-        if (p1.y > p2.y) {
-            var temp = p2;
-            p2 = p1;
-            p1 = temp;
+        if (v1.Coordinates.y > v2.Coordinates.y) {
+            var temp = v2;
+            v2 = v1;
+            v1 = temp;
         }
+        var p1 = v1.Coordinates;
+        var p2 = v2.Coordinates;
+        var p3 = v3.Coordinates;
+        var vnFace = (v1.Normal.add(v2.Normal.add(v3.Normal))).scale(1 / 3);
+        var centerPoint = (v1.WorldCoordinates.add(v2.WorldCoordinates.add(v3.WorldCoordinates))).scale(1 / 3);
+        var lightPos = new BABYLON.Vector3(0, 10, 10);
+        var ndotl = this.computeNDotL(centerPoint, vnFace, lightPos);
+        var data = { ndotla: ndotl };
         var dP1P2;
         var dP1P3;
-        //slope
         if (p2.y - p1.y > 0)
             dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
         else
             dP1P2 = 0;
         if (p3.y - p1.y > 0)
-            dP1P3 = (p2.x - p1.x) / (p3.y - p1.y);
+            dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
         else
             dP1P3 = 0;
         if (dP1P2 > dP1P3) {
             for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
-                if (y < p2.y)
-                    this.processScanLine(y, p1, p3, p1, p2, color);
-                else
-                    this.processScanLine(y, p1, p3, p2, p3, color);
+                data.currentY = y;
+                if (y < p2.y) {
+                    this.processScanLine(data, v1, v3, v1, v2, color);
+                }
+                else {
+                    this.processScanLine(data, v1, v3, v2, v3, color);
+                }
             }
         }
         else {
             for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
-                if (y < p2.y)
-                    this.processScanLine(y, p1, p2, p1, p3, color);
-                else
-                    this.processScanLine(y, p2, p3, p1, p3, color);
+                data.currentY = y;
+                if (y < p2.y) {
+                    this.processScanLine(data, v1, v2, v1, v3, color);
+                }
+                else {
+                    this.processScanLine(data, v2, v3, v1, v3, color);
+                }
             }
         }
     };
@@ -135,9 +165,9 @@ var Renderer = (function () {
                 var vertexA = cMesh.Vertices[currentFace.A];
                 var vertexB = cMesh.Vertices[currentFace.B];
                 var vertexC = cMesh.Vertices[currentFace.C];
-                var pixelA = this.project(vertexA, transformMatrix);
-                var pixelB = this.project(vertexB, transformMatrix);
-                var pixelC = this.project(vertexC, transformMatrix);
+                var pixelA = this.project(vertexA, transformMatrix, worldMatrix);
+                var pixelB = this.project(vertexB, transformMatrix, worldMatrix);
+                var pixelC = this.project(vertexC, transformMatrix, worldMatrix);
                 var color = 0.25 + ((indexFaces % cMesh.Faces.length) / cMesh.Faces.length) * 0.75;
                 this.drawTriangle(pixelA, pixelB, pixelC, new BABYLON.Color4(color, color, color, 1));
             }
@@ -181,7 +211,14 @@ var Renderer = (function () {
                 var x = verticesArray[index * verticesStep];
                 var y = verticesArray[index * verticesStep + 1];
                 var z = verticesArray[index * verticesStep + 2];
-                mesh.Vertices[index] = new BABYLON.Vector3(x, y, z);
+                var nx = verticesArray[index * verticesStep + 3];
+                var ny = verticesArray[index * verticesStep + 4];
+                var nz = verticesArray[index * verticesStep + 5];
+                mesh.Vertices[index] = {
+                    Coordinates: new BABYLON.Vector3(x, y, z),
+                    Normal: new BABYLON.Vector3(nx, ny, nz),
+                    WorldCoordinates: null
+                };
             }
             for (var index = 0; index < facesCount; index++) {
                 var a = indicesArray[index * 3];
--- engine.ts
+++ engine.ts
@@ -6,6 +6,20 @@ interface Face {
     C: number;
 }
 
+interface Vertex {
+    Normal: BABYLON.Vector3;
+    Coordinates: BABYLON.Vector3;
+    WorldCoordinates: BABYLON.Vector3;
+}
+
+interface ScanLineData {
+    currentY?: number;
+    ndotla?: number;
+    ndotlb?: number;
+    ndotlc?: number;
+    ndotld?: number;
+}
+
 class Cacamera {
     Position: BABYLON.Vector3;
     Target: BABYLON.Vector3;
@@ -19,7 +33,7 @@ class Cacamera {
 class Mesh {
     Position: BABYLON.Vector3;
     Rotation: BABYLON.Vector3;
-    Vertices: BABYLON.Vector3[];
+    Vertices: Vertex[];
     Faces: Face[];
 
     constructor(public name: string, verticesCount: number, facesCount: number) {
@@ -76,12 +90,18 @@ class Renderer {
         this.backbufferdata[index4 + 3] = color.a * 255;
     }
 
-    public project(coord: BABYLON.Vector3, transMat: BABYLON.Matrix): BABYLON.Vector3 {
-        var point = BABYLON.Vector3.TransformCoordinates(coord, transMat);
+    public project(vertex: Vertex, transMat: BABYLON.Matrix, world: BABYLON.Matrix): Vertex {
+        var point2d = BABYLON.Vector3.TransformCoordinates(vertex.Coordinates, transMat);
+        var point3DWorld = BABYLON.Vector3.TransformCoordinates(vertex.Coordinates, world);
+        var normal3DWorld = BABYLON.Vector3.TransformCoordinates(vertex.Normal, world);
 
-        var x = point.x * this.workingWidth + this.workingWidth / 2.0;
-        var y = -point.y * this.workingHeight + this.workingHeight / 2.0;
-        return (new BABYLON.Vector3(x, y, point.z));
+        var x = point2d.x * this.workingWidth + this.workingWidth / 2.0;
+        var y = -point2d.y * this.workingHeight + this.workingHeight / 2.0;
+        return ({
+            Coordinates: new BABYLON.Vector3(x, y, point2d.z),
+            Normal: normal3DWorld,
+            WorldCoordinates: point3DWorld
+        })
     }
 
     public drawPoint(point: BABYLON.Vector3, color: BABYLON.Color4): void {
@@ -98,9 +118,14 @@ class Renderer {
         return min + (max - min) * this.clamp(gradient);
     }
 
-    public processScanLine(y: number, pa: BABYLON.Vector3, pb: BABYLON.Vector3, pc: BABYLON.Vector3, pd: BABYLON.Vector3, color: BABYLON.Color4): void{
-        var gradient1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
-        var gradient2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+    public processScanLine(data: ScanLineData, va: Vertex, vb: Vertex, vc: Vertex, vd: Vertex, color: BABYLON.Color4): void{
+        var pa = va.Coordinates;
+        var pb = vb.Coordinates;
+        var pc = vc.Coordinates;
+        var pd = vd.Coordinates;
+
+        var gradient1 = pa.y != pd.y ? (data.currentY - pa.y) / (pd.y - pa.y) : 1;
+        var gradient2 = pc.y != pd.y ? (data.currentY - pc.y) / (pd.y - pc.y) : 1;
 
         var sx = this.interpolate(pa.x, pb.x, gradient1) >> 0;
         var ex = this.interpolate(pc.x, pd.x, gradient2) >> 0;
@@ -111,61 +136,88 @@ class Renderer {
         for (let x = sx; x < ex; x++) {
             var gradient: number = (x - sx) / (ex - sx);
             var z = this.interpolate(z1, z2, gradient);
-            this.drawPoint(new BABYLON.Vector3(x, y, z), color);
+            var ndotl = data.ndotla;
+            this.drawPoint(new BABYLON.Vector3(x, data.currentY, z), new BABYLON.Color4(color.r * ndotl, color.g * ndotl, color.b * ndotl, 1));
         }
     }
 
-    public drawTriangle(p1: BABYLON.Vector3, p2: BABYLON.Vector3, p3: BABYLON.Vector3, color: BABYLON.Color4): void {
-        if (p1.y > p2.y) {
-            var temp = p2;
-            p2 = p1;
-            p1 = temp;
+    public computeNDotL(vertex: BABYLON.Vector3, normal: BABYLON.Vector3, lightPosition: BABYLON.Vector3): number {
+        var lightDirection = lightPosition.subtract(vertex);
+        normal.normalize();
+        lightDirection.normalize();
+
+        return Math.max(0, BABYLON.Vector3.Dot(normal, lightDirection));
+    }
+
+    public drawTriangle(v1: Vertex, v2: Vertex, v3: Vertex, color: BABYLON.Color4): void {
+        if (v1.Coordinates.y > v2.Coordinates.y) {
+            var temp = v2;
+            v2 = v1;
+            v1 = temp;
         }
 
-        if (p2.y > p3.y) {
-            var temp = p2;
-            p2 = p3;
-            p3 = temp;
+        if (v2.Coordinates.y > v3.Coordinates.y) {
+            var temp = v2;
+            v2 = v3;
+            v3 = temp;
         }
 
-        if (p1.y > p2.y) {
-            var temp = p2;
-            p2 = p1;
-            p1 = temp;
+        if (v1.Coordinates.y > v2.Coordinates.y) {
+            var temp = v2;
+            v2 = v1;
+            v1 = temp;
         }
+
+        var p1 = v1.Coordinates;
+        var p2 = v2.Coordinates;
+        var p3 = v3.Coordinates;
+
+        var vnFace = (v1.Normal.add(v2.Normal.add(v3.Normal))).scale(1 / 3);
+        var centerPoint = (v1.WorldCoordinates.add(v2.WorldCoordinates.add(v3.WorldCoordinates))).scale(1 / 3);
+
+        var lightPos = new BABYLON.Vector3(0, 10, 10);
+
+        var ndotl = this.computeNDotL(centerPoint, vnFace, lightPos);
+
+        var data: ScanLineData = { ndotla: ndotl };
 
         var dP1P2: number;
         var dP1P3: number;
 
-        //slope
         if (p2.y - p1.y > 0)
             dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
         else
             dP1P2 = 0;
 
         if (p3.y - p1.y > 0)
-            dP1P3 = (p2.x - p1.x) / (p3.y - p1.y);
+            dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
         else
             dP1P3 = 0;
 
         if (dP1P2 > dP1P3) {
-            for (let y = p1.y >> 0; y <= p3.y >> 0; y++) {
-                if (y < p2.y)
-                    this.processScanLine(y, p1, p3, p1, p2, color);
-                else
-                    this.processScanLine(y, p1, p3, p2, p3, color);
+            for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                data.currentY = y;
+
+                if (y < p2.y) {
+                    this.processScanLine(data, v1, v3, v1, v2, color);
+                }
+                else {
+                    this.processScanLine(data, v1, v3, v2, v3, color);
+                }
             }
         }
         else {
-            for (let y = p1.y >> 0; y <= p3.y >> 0; y++) {
-                if (y < p2.y)
-                    this.processScanLine(y, p1, p2, p1, p3, color);
-                else
-                    this.processScanLine(y, p2, p3, p1, p3, color);
+            for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                data.currentY = y;
+
+                if (y < p2.y) {
+                    this.processScanLine(data, v1, v2, v1, v3, color);
+                }
+                else {
+                    this.processScanLine(data, v2, v3, v1, v3, color);
+                }
             }
         }
-
-
     }
 
     public render(cacamera: Cacamera, meshes: Mesh[]): void {
@@ -184,9 +236,9 @@ class Renderer {
                 var vertexB = cMesh.Vertices[currentFace.B];
                 var vertexC = cMesh.Vertices[currentFace.C];
 
-                var pixelA = this.project(vertexA, transformMatrix);
-                var pixelB = this.project(vertexB, transformMatrix);
-                var pixelC = this.project(vertexC, transformMatrix);
+                var pixelA = this.project(vertexA, transformMatrix, worldMatrix);
+                var pixelB = this.project(vertexB, transformMatrix, worldMatrix);
+                var pixelC = this.project(vertexC, transformMatrix, worldMatrix);
 
                 var color: number = 0.25 + ((indexFaces % cMesh.Faces.length) / cMesh.Faces.length) * 0.75;
                 this.drawTriangle(pixelA, pixelB, pixelC, new BABYLON.Color4(color, color, color, 1));
@@ -236,7 +288,16 @@ class Renderer {
                 var x = verticesArray[index * verticesStep];
                 var y = verticesArray[index * verticesStep + 1];
                 var z = verticesArray[index * verticesStep + 2];
-                mesh.Vertices[index] = new BABYLON.Vector3(x, y, z);
+
+                var nx = verticesArray[index * verticesStep + 3];
+                var ny = verticesArray[index * verticesStep + 4];
+                var nz = verticesArray[index * verticesStep + 5];
+
+                mesh.Vertices[index] = {
+                    Coordinates: new BABYLON.Vector3(x, y, z),
+                    Normal: new BABYLON.Vector3(nx, ny, nz),
+                    WorldCoordinates: null
+                };
             }
 
             for (let index = 0; index < facesCount; index++) {
